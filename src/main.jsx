69559a9b028d78ac6a9c04336429707099820c36
Mohamed Adel <mohamedadelhareidy@gmail.com>
2026-04-26 19:37:55 +0300

    ...

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 import './index.css'
 import App from './App.jsx'
--- src/modules/Shared/Components/Navbar/Navbar.jsx
+++ src/modules/Shared/Components/Navbar/Navbar.jsx
@@ -1,57 +1,93 @@
 import React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import Menu from "@mui/material/Menu";
+import MenuIcon from "@mui/icons-material/Menu";
+import Container from "@mui/material/Container";
+import Avatar from "@mui/material/Avatar";
+import MenuItem from "@mui/material/MenuItem";
+import Tooltip from "@mui/material/Tooltip";
+
 import Logo from "../../../../assets/images/Logo/axis logo.svg";
 import style from "./Navbar.module.css";
-import { Link } from "react-router-dom";
+
+const settings = ["Profile", "Account", "Dashboard", "Logout"];
 
 export default function Navbar() {
+  const [anchorElUser, setAnchorElUser] = React.useState(null);
+  const [anchorElNav, setAnchorElNav] = React.useState(null);
+
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  const handleOpenUserMenu = (event) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
+
   return (
-    <nav className="navbar navbar-light">
-      <div className="container-fluid d-flex justify-content-between align-items-center">
-        {/* Logo */}
-        <div className={style.image}>
-          <Link className="navbar-brand" to="/">
-            <img src={Logo} className={style.logo} alt="logo" />
-          </Link>
-        </div>
-
-        {/* User Dropdown */}
-        <div className="dropdown">
-          <button
-            className="btn btn-light dropdown-toggle d-flex align-items-center gap-2"
-            data-bs-toggle="dropdown"
+    <AppBar position="static">
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          <Typography
+            component="a"
+            href="/"
+            sx={{ display: "flex", alignItems: "center" }}
           >
-            <img
-              src="https://github.com/mdo.png"
-              alt="user"
-              width="32"
-              height="32"
-              className="rounded-circle"
-            />
-            <span>Admin</span>
-          </button>
+            <img className={style.logo} src={Logo} alt="logo" />
+          </Typography>
 
-          <ul className="dropdown-menu dropdown-menu-end shadow">
-            <li>
-              <Link className="dropdown-item" to="/profile">
-                Profile
-              </Link>
-            </li>
-            <li>
-              <Link className="dropdown-item" to="/settings">
-                Settings
-              </Link>
-            </li>
-            <li>
-              <hr className="dropdown-divider" />
-            </li>
-            <li>
-              <Link className="dropdown-item text-danger" to="/logout">
-                Logout
-              </Link>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </nav>
+          <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
+            <IconButton
+              size="large"
+              onClick={handleOpenNavMenu}
+              color="inherit"
+            >
+              <MenuIcon />
+            </IconButton>
+
+            <Menu
+              anchorEl={anchorElNav}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+            >
+              <MenuItem onClick={handleCloseNavMenu}>Home</MenuItem>
+              <MenuItem onClick={handleCloseNavMenu}>Dashboard</MenuItem>
+            </Menu>
+          </Box>
+
+          <Box sx={{ flexGrow: 0, ml: "auto" }}>
+            <Tooltip title="Open settings">
+              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                <Avatar alt="Admin" src="/static/images/avatar/2.jpg" />
+              </IconButton>
+            </Tooltip>
+
+            <Menu
+              anchorEl={anchorElUser}
+              open={Boolean(anchorElUser)}
+              onClose={handleCloseUserMenu}
+            >
+              {settings.map((setting) => (
+                <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                  {setting}
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
   );
 }
